a9cb72cfdeec7813a1c09445271440b7b3fdeb23
kirillOff <kirilloffkostya@gmail.com>
2022-04-27 20:43:53 +0300

    Initial Commit

---
+++ ArtMaker_app/ArtMaker_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtMaker_appApp.swift
+//  ArtMaker_app
+//
+//  Created by Konstantin Kirillov on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ArtMaker_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
